abceae47bf22711863bbd956f28114cba3e90036
Ally <ally@primeacademy.io>
2019-06-21 13:27:18 -0500

    setting up html form

--- index.php
+++ index.php
@@ -1,2 +1,21 @@
-<?php
-    echo "Hello world!";
+<!DOCTYPE html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Hello PHP!</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <script src="https://code.jquery.com/jquery-3.4.1.min.js" 
+        integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
+        crossorigin="anonymous"></script>
+        <script src="client.js"></script>
+    </head>
+    <body>
+        <?php
+            echo "<h1>Welcome to the record store!</h1>"
+        ?>
+        <h2>Add a new record:</h2>
+        <input type="text" placeholder="Album Name" id="newAlbumName" />
+        <input type="text" placeholder="Artist" id="newAlbumArtist" />
+        <button id="addNewRecord">Add Album</button>
+    </body>
+</html>
